5cfa3203fc4d5dd1407abb4fe396a554ccbc55f3
Horita <ryohorita105@gmail.com>
2024-02-08 11:47:00 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -379,7 +379,7 @@ else:
     areaname="East"
 
 # 図のSIZE指定inch                                                                        
-fig = plt.figure(figsize=(16,12))
+fig = plt.figure(figsize=(8,6))
 # 余白設定                                                                                
 plt.subplots_adjust(left=0.04, right=1.1, bottom=0.0, top=1.0)                  
 # 作図                                                                                    
