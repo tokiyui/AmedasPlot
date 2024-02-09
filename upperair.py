5ed1c450fa25a5acfc4461ae492f0464013c02d4
Horita <ryohorita105@gmail.com>
2024-02-09 10:11:02 +0900

    Update upperair.py

--- upperair.py
+++ upperair.py
@@ -93,10 +93,10 @@ nc_file = nc.Dataset(basename, 'r')
 print("read")
 
 # 緯度、経度、およびデータの取得
-print("data")
-latitude = nc_file.variables['latitude'][:]
-longitude = nc_file.variables['longitude'][:]
-data = nc_file.variables['tbb_13'][:]
+#print("data")
+#latitude = nc_file.variables['latitude'][:]
+#longitude = nc_file.variables['longitude'][:]
+#data = nc_file.variables['tbb_13'][:]
 
 # メッシュグリッドを作成
 print("grid")
@@ -132,7 +132,7 @@ plt.clabel(cont, fontsize=15)
 #cb.ax.tick_params(labelsize=8)
 
 # 描画
-plt.contourf(lon, lat, data.reshape((2401, 2401)), cmap='gray_r', levels=np.arange(180, 331, 10))
+#plt.contourf(lon, lat, data.reshape((2401, 2401)), cmap='gray_r', levels=np.arange(180, 331, 10))
 #plt.title('Brightness Temperature - Band tbb_08')
 
 # ベクトルの間引き間隔
