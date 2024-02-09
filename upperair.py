ad30e093af8ade8fa18379a1b5492fdff772cc35
Horita <ryohorita105@gmail.com>
2024-02-09 14:43:23 +0900

    Update upperair.py

--- upperair.py
+++ upperair.py
@@ -115,7 +115,7 @@ proj = ccrs.PlateCarree()
 fig = plt.figure(figsize=(8,6))
 # 余白設定                                                                                
 #plt.subplots_adjust(left=0.04, right=1.05, bottom=0.0, top=1.0)   
-plt.subplots_adjust(left=0.04, right=1.0, bottom=0.0, top=0.98) 
+plt.subplots_adjust(left=0.04, right=0.9, bottom=0.0, top=0.98) 
 # 作図                                                                                    
 ax = fig.add_subplot(1, 1, 1, projection=proj)
 ax.set_extent([120, 150, 22.4, 47.6], proj)
