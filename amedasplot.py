97d5408ec88b17ef465377b3406e20bafcd93dd0
Horita <ryohorita105@gmail.com>
2024-02-13 14:22:52 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -882,8 +882,8 @@ cont = plt.contour(grid_lon_p, grid_lat_p, tmp850, levels=np.arange(-60, 60, 3),
 #ax.streamplot(grid_lon_p, grid_lat_p, u850, v850, linewidth=2, density=1, color="purple")
 
 
-plt.fill_between(lons, lats, where=(ttd <= 3), color='lightgreen', alpha=0.3)
-plt.fill_between(lons, lats, where=(ttd >= 15), color='yellow', alpha=0.3)
+plt.fill_between(lons, lats, where=(ttd.reshape((241, 253)) <= 3), color='lightgreen', alpha=0.3)
+plt.fill_between(lons, lats, where=(ttd.reshape((241, 253)) >= 15), color='yellow', alpha=0.3)
 
 # 海岸線
 ax.coastlines(resolution='10m', linewidth=1.6, color='black')  
