001b1ac30ec094a803679dacd76f6024988513b8
Horita <ryohorita105@gmail.com>
2024-02-19 23:21:20 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -689,7 +689,7 @@ lons = np.arange(120, 150 + 0.0625, 0.125)
 dx, dy = mpcalc.lat_lon_grid_deltas(lons, lats)
 #ug300, vg300 = mpcalc.geostrophic_wind(height300 * units('m'), dx=dx, dy=dy, latitude=lats, longitude=lons)
 
-ug500, vg500 = mpcalc.geostrophic_wind(height500 * units('m'), dx=dx, dy=dy, latitude=lat)
+ug500, vg500 = mpcalc.geostrophic_wind(height500 * units('m'), dx=dx, dy=dy, latitude=grid_lat_p)
 
 
 # height500の形状を確認する
