7949a00dec7c955e4de23dcd560b8d8453332b0f
Horita <ryohorita105@gmail.com>
2024-02-19 17:12:16 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -707,7 +707,8 @@ print("latitudeの形状:", lats.shape)
 
 
 # ug500, vg500 = mpcalc.geostrophic_wind(height500 * units('m'), dx=dx, dy=dy, latitude=lats)
-# vor = mpcalc.vorticity(ug500 * units('m/s'), vg500 * units('m/s'), dx=dx, dy=dy) * 1000000
+
+vor = mpcalc.vorticity(u500 * units('m/s'), v500 * units('m/s'), dx=dx, dy=dy) * 1000000
 fg = mpcalc.frontogenesis(ept925 * units('K'), u925 * units('m/s'), v925 * units('m/s'), dx=dx, dy=dy)
 
 ### 300hPa ###
