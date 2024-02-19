48b3a52948fb1b7006c302eea886dc32d6d7c70e
Horita <ryohorita105@gmail.com>
2024-02-19 10:24:39 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -689,6 +689,7 @@ lons = np.arange(120, 150 + 0.0625, 0.125)
 dx, dy = mpcalc.lat_lon_grid_deltas(lons, lats)
 # ug300, vg300 = mpcalc.geostrophic_wind(height300 * units('m'), dx=dx, dy=dy, latitude=lats, longitude=lons)
 ug500, vg500 = mpcalc.geostrophic_wind(height500 * units('m'), dx=dx, dy=dy)
+ug500, vg500 = mpcalc.geostrophic_wind(height500 * units('m'), dx=dx, dy=dy, latitude=lats)
 vor = mpcalc.vorticity(ug500 * units('m/s'), vg500 * units('m/s'), dx=dx, dy=dy) * 1000000
 fg = mpcalc.frontogenesis(ept925, u925 * units('m/s'), v925 * units('m/s'), dx=dx, dy=dy)
 
