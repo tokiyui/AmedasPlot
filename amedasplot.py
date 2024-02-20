3a0c84bb9fbfca9c2170b88347ae30c1124072f6
Horita <ryohorita105@gmail.com>
2024-02-20 11:21:16 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -696,10 +696,10 @@ ug500 = gaussian_filter(ug500, sigma=4.0)
 vg500 = gaussian_filter(vg500, sigma=4.0)
 
 vor = mpcalc.vorticity(ug500* units('m/s'), vg500* units('m/s'), dx=dx, dy=dy) * 1000000
-vor = gaussian_filter(ug300, sigma=4.0)
+vor = gaussian_filter(vor, sigma=8.0)
 fg = mpcalc.frontogenesis(ept925 * units('K'), u925 * units('m/s'), v925 * units('m/s'), dx=dx, dy=dy) * 10000000000
 max_value = np.max(fg)
-fg = gaussian_filter(fg, sigma=8.0)
+fg = gaussian_filter(fg, sigma=4.0)
 
 ### 300hPa ###
 # 作図                                                                                    
@@ -722,7 +722,7 @@ plt.tight_layout(rect=[0, 0, 1, 0.96])
 
 # 風速の計算
 wind_speed = mpcalc.wind_speed(ug300* units('m/s'), vg300* units('m/s')).to(units.knots)
-wind_speed = gaussian_filter(wind_speed, sigma=4.0)
+wind_speed = gaussian_filter(wind_speed, sigma=8.0)
 plt.contourf(grid_lon_p, grid_lat_p, wind_speed, levels=[0, 60, 120, np.inf], colors=['none', 'blue', 'purple'], alpha=0.2)
 
 # 海岸線
