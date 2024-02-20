bd7bbdf8e932b5bc4bfb2f53b2f23553d2686d7b
Horita <ryohorita105@gmail.com>
2024-02-20 10:21:54 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -810,7 +810,7 @@ gl.ylocator = mticker.FixedLocator(np.arange(-90,90,5))
 cont = plt.contour(grid_lon_p, grid_lat_p, ept850, levels=np.arange(210, 390, 3), linewidths=2, linestyles='solid', colors='green')
 plt.clabel(cont, fontsize=15)
 
-plt.contourf(grid_lon_p, grid_lat_p, fg, levels=[-float('inf'), 2000000000000, np.inf], colors=['none', 'orange'])
+plt.contourf(grid_lon_p, grid_lat_p, fg, levels=[-float('inf'), 20000, np.inf], colors=['none', 'orange'])
 
 # ベクトルの間引き間隔
 stride = 5
