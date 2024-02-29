af9a142c1ecf00458f3d18c9e20130e61006a87b
Horita <ryohorita105@gmail.com>
2024-03-01 08:53:55 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -672,9 +672,9 @@ ept925 = gaussian_filter(ept925, sigma=4.0)
 u925 = gaussian_filter(u925, sigma=4.0)
 v925 = gaussian_filter(v925, sigma=4.0)
 
-print(ft,utc)
+print(ft,time)
 data_wv, lon, lat = read_hima(ft, '08')
-data_ir, lon, lat = read_hima(utc, '13')
+data_ir, lon, lat = read_hima(time, '13')
 
 # 図法指定                                                                             
 proj = ccrs.PlateCarree()                                                      
