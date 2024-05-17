3acf9db79674d2b2aa5e39879038465f11dbd629
Horita <ryohorita105@gmail.com>
2024-05-17 12:27:26 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -561,7 +561,7 @@ for area in [0, 1, 2, 3]:
     #diff_temp[sealand_filterd > 0] = grid_temp[sealand_filterd > 0] - tmp[sealand_filterd > 0]
     
     grid_npre = prmsl + diff_npre
-    grid_temp = tmp + diff_temp
+    #grid_temp = tmp + diff_temp
     diff_npre = gaussian_filter(diff_npre, sigma=2.0)
     diff_temp = gaussian_filter(diff_temp, sigma=2.0) 
 
