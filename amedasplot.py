e3c3677eabc511b2e6a0fd5a81e1e740bbd7c6fa
Horita <ryohorita105@gmail.com>
2024-05-21 11:47:05 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -545,9 +545,6 @@ for area in [0, 1, 2, 3]:
     grid_npre = np.where(np.isnan(grid_npre), prmsl, grid_npre)
     grid_npre = gaussian_filter(grid_npre, sigma=4.0)
 
-    print("grid_temp の形状:", grid_temp.shape)
-    print("tmp の形状:", tmp.shape)
-
     prmsl = gaussian_filter(prmsl, sigma=2.0)
     tmp = gaussian_filter(tmp, sigma=2.0)
     #diff_temp = 0 #(grid_temp - tmp) * sealand_filterd / 10000.0
@@ -561,7 +558,7 @@ for area in [0, 1, 2, 3]:
     
     grid_npre = prmsl + diff_npre
     grid_temp = tmp #+ diff_temp
-    #diff_npre = gaussian_filter(diff_npre, sigma=10.0)
+    diff_npre = gaussian_filter(diff_npre, sigma=2.0)
     #diff_temp = gaussian_filter(diff_temp, sigma=2.0) 
 
     #陸地から十分離れた格子は描画しない(MSMと実況の差が大きい場合があるため)
