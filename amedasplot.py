8e1834245f32d00e95e3ccdbad3ffd6dec15492b
Horita <ryohorita105@gmail.com>
2024-06-17 22:06:10 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -366,9 +366,9 @@ data_url = "https://www.jma.go.jp/bosai/jmatile/data/rasrf/{}/immed/{}/surf/rasr
 data_url=data_url.format(utc.strftime("%Y%m%d%H%M"),utc.strftime("%Y%m%d%H%M"))
 
 # データの取得
-response = requests.get(data_url)
-data = json.loads(response.text)
- 
+response = urlopen(url_data_json)
+data = response.read()
+
 # 座標と値のリストを作成
 coordinates = []
 values = []
