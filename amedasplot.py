0e13bd9cb4b4d11c25876a6fc9116169f34fdc58
Horita <ryohorita105@gmail.com>
2024-11-28 23:27:39 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -857,7 +857,8 @@ ttd850 = (tmp850 - mpcalc.dewpoint_from_relative_humidity((tmp850+273.15) * unit
 ttd925 = (tmp925 - mpcalc.dewpoint_from_relative_humidity((tmp925+273.15) * units('K'), rh925 / 100).magnitude)
 kindex58 = tmp850 - tmp500 + mpcalc.dewpoint_from_relative_humidity((tmp850+273.15) * units('K'), rh850 / 100).magnitude - ttd700
 kindex79 = tmp925 - tmp700 + mpcalc.dewpoint_from_relative_humidity((tmp925+273.15) * units('K'), rh925 / 100).magnitude - ttd850
-ssi = tmp500 - mpcalc.parcel_profile([850, 500] * units.hPa, tmp850 * units.degC, (tmp850 + ttd850) * units.degC).to('degC')[1].m
+pressure_levels = np.array([[850, 500]] * tmp850.shape[0] * tmp850.shape[1]).reshape(tmp850.shape[0], tmp850.shape[1], 2) * units.hPa
+ssi = tmp500 - mpcalc.parcel_profile(pressure_levels, tmp850 * units.degC, (tmp850 + ttd850) * units.degC).to('degC')[1].m
 ssi_winter = tmp700 - mpcalc.parcel_profile([925, 700] * units.hPa, tmp925 * units.degC, (tmp925 + ttd925) * units.degC).to('degC')[1].m
 ssi[(tmp700 + 273.15) < -20.0] = ssi_winter[(tmp700 + 273.15) < -20.0]
 
