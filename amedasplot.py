f351f9ac0413cc5e11f34813b5156c4e79439c34
Horita <ryohorita105@gmail.com>
2024-12-25 11:28:19 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -525,6 +525,7 @@ if header_row:
                     lon_list_w.append(lon)
                     wind_x_components.append(wx)
                     wind_y_components.append(wy)
+                    print(lat, lon, wx, wy)
 
 # 前1時間の雷実況
 for i in range(1,12):
