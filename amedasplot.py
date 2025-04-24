6cee62bc3091478556f8dd55b34b3891ea1fe04f
Horita <ryohorita105@gmail.com>
2025-04-24 12:28:48 +0900

    Update amedasplot.py

--- amedasplot.py
+++ amedasplot.py
@@ -500,7 +500,6 @@ wind_y_components = []
 if header_row:
     for data in ship_data[ship_data.index(header_row) + 1:]:
         data_str = data.text.split()
-        print(len(data_str), data_str[0], data_str[1], "{}".format(time.strftime("%H")))
         if len(data_str) >= 22 and "SHIP" in data_str[0] and "{}".format((time - offsets.Hour(9)).strftime("%H")) in data_str[1]:
             lat = float(data_str[2])
             lon = float(data_str[3])
